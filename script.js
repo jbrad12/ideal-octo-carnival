21049d862ee5acb45510b6bffb48635309b24179
Jacob Bradshaw <jakebradshaw123@gmail.com>
2020-10-31 13:05:22 -0700

    Added index.html , script.js , and style.css

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $(".row").each(function () {
+        var timeBlock = parseInt($(this).attr("id"))
+        if (timeBlock < moment().format('HH')) {
+            $(this).addClass("past")
+        }
+
+        if (timeBlock == moment().format('HH')) {
+            $(this).addClass("present")
+        }
+
+        if (timeBlock > moment().format('HH')) {
+            $(this).addClass("future")
+        }
+
+    
+
+    })
+
+    $(".saveBtn").on("click", function(){
+    var input = $(this).siblings(".time-block").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time, input)
+
+    })
+    var keys = Object.keys(localStorage);
+
+    for (let i = 0; i < keys.length; i++) {
+        var value = localStorage.getItem(keys[i]);
+        var display = $("#" + keys[i]).find("textarea")
+        display.text(value);
+    }
+})
